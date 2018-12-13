c8d7de04ee7375f2b1fef5e5cf3102b1050e6254
ppllelp <42909442+ppllelp@users.noreply.github.com>
2018-12-13 14:42:17 +0700

    Delete 2.py

--- 2.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-x = np.arange(1,12)
-y = [22,20,14,12,11,6,5,4,3,2,1]
-mueang = ['KOI Thé','Ochaya','Mikucha','Mr. Shake','Kamu','Coco','ATM','The alley','Dakashi','Moma’s','CHA BAR']
-ax = plt.gca(xticks=x) 
-ax.set_xticklabels(mueang,rotation=10) 
-plt.bar(x,y)
-plt.show()
